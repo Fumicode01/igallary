7cc41150329e0827a05fcd8e156d71010af06b77
Fumihito <65881354+Fumicode01@users.noreply.github.com>
2022-02-15 21:33:41 +1100

    Added main title section

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react'
 import Header from '../header/Header'
+import MainTitle from '../mainTitle/MainTitle'
 
 const Layout = (props:any) => {
   return (
         <Fragment>
             <Header />
+            <MainTitle />
             <main>{props.children}</main>
         </Fragment>
   )
---
+++ components/mainTitle/MainTitle.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import classes from './mainTitle.module.css'
+
+const MainTitle = () => {
+  return (
+      <section className={classes.main_title_section}>
+          <h1>My Gallary</h1>
+          <h2>FInd your favourite pictures!</h2>
+      </section>
+  )
+}
+
+export default MainTitle
