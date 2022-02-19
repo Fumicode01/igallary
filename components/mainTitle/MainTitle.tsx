d1f7b76489e766b336689313e13f436b06ed16c3
Fumihito <65881354+Fumicode01@users.noreply.github.com>
2022-02-19 22:02:34 +1100

    Fixed type error

--- components/mainTitle/MainTitle.tsx
+++ components/mainTitle/MainTitle.tsx
@@ -5,7 +5,7 @@ const MainTitle = () => {
   return (
       <section className={classes.main_title_section}>
           <h1>My Gallary</h1>
-          <h2>FInd your favourite pictures!</h2>
+          <h2>Find your favourite pictures!</h2>
       </section>
   )
 }
