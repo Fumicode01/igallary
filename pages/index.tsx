dc383e1f60fd5d0fd4f0c815fe7bd914a516af80
Fumihito <65881354+Fumicode01@users.noreply.github.com>
2022-02-20 14:07:05 +1100

    Fixed the error for changing pages in pagination
    Fixed the page error when searchWord is changed pagination would not work

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useLayoutEffect, useContext, useRef } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import type { NextPage } from 'next'
 import axios from 'axios'
 import BoxLoading from 'react-loading';
@@ -27,47 +27,28 @@ const Home: NextPage = () => {
     const [totalPages, setTotalPages] = useState(0);
     const [isLoading, setIsLoading] = useState(false);
     const [selectedImage, setSelectedImage] = useState<Image | null>(null);
-    const firstUpdate = useRef(true);
 
 
-    const { state, dispatch } = useContext(configContext);
+    const { state } = useContext(configContext);
     const searchWord = state.searchWord;
 
     useEffect(() => {
         const fetchImages = async () => {
             setIsLoading(true);
-            const response = await axios.get(`https://api.unsplash.com/photos?page=${page}&per_page=9`, config);
+            const response =  searchWord ? await axios.get(`https://api.unsplash.com/search/photos?page=${page}&query=${searchWord}&per_page=9`, config) : await axios.get(`https://api.unsplash.com/photos?page=${page}&per_page=9`, config);
             const maxItem = parseInt(response.headers.link.split('=')[1].split('&')[0]);
             totalPages == 0 ? setTotalPages(Math.ceil(Number(maxItem) / 9)) : setTotalPages(totalPages);
-
-
-            await setImages(response.data);
+            searchWord ? setImages(response.data.results) : setImages(response.data);
             setTimeout(() => {
                 setIsLoading(false);
             },1000)
         }
         fetchImages();
-    } , [page]);
+    } , [page, searchWord]);
 
-    useLayoutEffect(() => {
-        if(firstUpdate.current){
-            firstUpdate.current = false;
-            return
-        }
-        const fetchImages = async () => {
-            setIsLoading(true);
-            setPage(1)
-            const response = await axios.get(`https://api.unsplash.com/search/photos?page=1&query=${searchWord}&per_page=9`, config);
-            setTotalPages(response.data.total_pages);
-
-
-            await setImages(response.data.results);
-            setTimeout(() => {
-                setIsLoading(false);
-            },1000)
-        }
-        fetchImages();
-    } , [searchWord]);
+    useEffect(()=> {
+        setPage(1);
+    }, [searchWord])
 
     const handlePages = (updatePage:number) => setPage(updatePage)
 
@@ -91,7 +72,7 @@ const Home: NextPage = () => {
         <Modal selectedImage={selectedImage} setSelectedImage={setSelectedImage} />
          )} */}
 
-{isLoading 
+        {isLoading 
             ? <BoxLoading className="loading" type="spin" color="#f0a5a0" height={100} width={100} /> 
             : (
                 <>
@@ -105,7 +86,7 @@ const Home: NextPage = () => {
                 </>
                 )}
                 { selectedImage && (
-        <Modal selectedImage={selectedImage} setSelectedImage={setSelectedImage} />
+             <Modal selectedImage={selectedImage} setSelectedImage={setSelectedImage} />
          )} 
     </div>
   )
