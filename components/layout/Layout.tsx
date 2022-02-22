cff87c64e65b158070fdaff7dfbd890d7f62bc7e
Fumihito <65881354+Fumicode01@users.noreply.github.com>
2022-02-23 00:48:17 +1100

    UPDATED | Updated global font to Noto serif

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -13,6 +13,10 @@ export const Layout = (props:any) => {
                 <Head>
                     <title>My Gallary</title>
                     <link rel="shortcut icon" href="/logo.png" />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Noto+Serif:wght@400;700&display=swap"
+                        rel="stylesheet"
+                    />
                 </Head>
                 <Header />
                 <main>{props.children}</main>
