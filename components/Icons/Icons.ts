30cf38985889abc9a724a1ed6d800db84617183d
Fumihito <65881354+Fumicode01@users.noreply.github.com>
2022-02-24 11:10:35 +1100

    FIXED | Delete components folder due to issue

--- components/Icons/Icons.ts
+++
@@ -1,9 +0,0 @@
-import { IconProp } from '@fortawesome/fontawesome-svg-core';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
-
-
-export const faHeartIcon = faHeart as IconProp
-export const faTwitterIcon = faTwitter as IconProp
-export const faInstagramIcon = faInstagram as IconProp
