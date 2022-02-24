722de5e7d83dbd53e733e67f3d54f51210e8b4ee
Fumihito <65881354+Fumicode01@users.noreply.github.com>
2022-02-24 11:35:48 +1100

    INSTALLED | sass-loader

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import  Link from 'next/link'
 import Image from 'next/image'
 
-
 import styles from './Header.module.scss'
 
 export const Header = () => {
--- components/MainTitle/MainTitle.tsx
+++ components/MainTitle/MainTitle.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import classes from './MainTitle.module.scss'
+import styles from './MainTitle.module.scss'
 
 export const MainTitle = () => {
   return (
-      <section className={classes.main_title_section}>
+      <section className={styles.main_title_section}>
           <h1>My Gallary</h1>
           <h2>Find your favourite images!</h2>
       </section>
