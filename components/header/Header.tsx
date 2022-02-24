08113a0903c8376e73f165df08af4ef97ca91503
Fumihito <65881354+Fumicode01@users.noreply.github.com>
2022-02-24 11:02:49 +1100

    Revert "Delete components directory"
    This reverts commit 2b64c19bb861e5d226268bd3a8af443de0a39767.

---
+++ components/header/Header.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import  Link from 'next/link'
+import Image from 'next/image'
+
+
+import styles from './header.module.scss'
+
+export const Header = () => {
+  return (
+      <nav>
+            <div className={styles.nav_wrapper}>
+                <Link href="/" >
+                    <Image width={90} height={90} src="/logo.png" />
+                </Link>
+                <div className={styles.header_right_container}>
+                    <ul className={styles.list_container}>
+                        <li>
+                            <Link href="/"><a className={styles.link_text}>Home</a></Link>
+                        </li>
+                        <li>
+                            <Link href="/about"><a className={styles.link_text}>About</a></Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+      </nav>
+  )
+}
---
+++ components/toggle/Toggle.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import styles from './toggle.module.scss'
+
+interface Props {
+    onClick: (e:any) => void
+    viewChange:boolean
+}
+
+export const Toggle = ({onClick, viewChange}:Props) => {
+    
+  return (
+      <div className={styles.container}>
+        <label className={styles.wrapper}>
+            <p>Switch View</p>
+            <input className={styles.input} type="checkbox" onChange={()=>onClick(!viewChange)} checked={viewChange}/>
+            <span className={styles.slider}></span>
+        </label>
+      </div>
+
+  )
+}
